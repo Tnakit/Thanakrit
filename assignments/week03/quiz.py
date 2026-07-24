712317bc8ab7d30bb4a87978b257cb9ae61eae63
Thanakrit Boonpit <thanakrit.boonp@ku.th>
2026-07-24 20:36:30 +0700

    ค้าบ

--- assignments/week03/quiz.py
+++ assignments/week03/quiz.py
@@ -8,7 +8,14 @@ age = int(input("Enter age: "))
 # 60+: Senior
 
 # Your code here:
-
+if age <= 12:
+    print("Child")
+elif age <= 19:
+    print("Teenager")
+elif age <= 59:
+    print("Adult")
+else:
+    print("Senior")
 
 
 # Complete this ATM simulation
