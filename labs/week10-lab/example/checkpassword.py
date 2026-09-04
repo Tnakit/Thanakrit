267edc37449ca517f5d352fdc123a09206820ec2
Thanakrit Boonpit <thanakrit.boonp@ku.th>
2026-09-04 09:58:38 +0700

    ดีครับจารย์

---
+++ labs/week10-lab/example/checkpassword.py
@@ -0,0 +1,10 @@
+# รับค่า password จากผู้ใช้
+# password นั้นแข็งแรงหรือไม่
+# password นั้นจะแข็งแรงถ้าประกอบไปด้วยตัวเลข ตัวอักษร มี '@' 1 ตัว ยาวมากกว่า 8 ตัว
+
+# ตัวอย่างหน้าจอ
+# Please input your password: Boonchoo
+# Your password not strong!
+
+# Please input your passsword : Bo@o15thai
+# Your password is strong!
---
+++ labs/week10-lab/example/count-letter.py
@@ -0,0 +1,19 @@
+#เขียนโปรแกรม
+# 1 รับค่าข้อความจากผู้ใช้ เป็นแปร str ชื่อ text
+# 2 รับค่าอักขระที่ต้องการนับในข้อความ text
+# 3 ดำเนินการนับอักขระตามที่ผู้ใช้ต้องการ และแสดงออกทางหน้าจอ
+
+# ตัวอย่างหน้าจอ
+# Input your text : Boonchoo Jitnupong
+# Which character do you want to count: o
+# 5 letters 'o' found in Boonchoo Jitnupong
+
+print("\n=== ITERATING THROUGH STRING ===")
+count = 0
+text = input('Input your text: ')
+x = input('Which character do you want to count: ')
+for letter in text:
+    if letter == :
+    count += 1
+    print(f"{count} letters 'l' found in '{text}'")
+    
